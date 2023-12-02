3bf41e631b6f6e114d9d838dc3b1259725c24db3
bahaashk <bahaa.shk@hotmail.com>
2023-12-03 00:55:05 +0200

    few small changes

--- src/data.js
+++ src/data.js
@@ -107,8 +107,8 @@ export const footerLinksColumnTwo = [
   {
     title: "Address",
     links: [
-      "JL. Setiabudhi No. 193 Sukasari, Bandung West Java, Indonesia",
-      "hallo@daunku.com",
+      "Beirut, Lebanon",
+      "Test@practice.com",
     ],
   },
 ];
